e4ddc7837af53ee962beaa10781eace3bd870c02
DoodleForce <random1042069@gmail.com>
2026-07-17 14:39:17 -0400

    Add files via upload

---
+++ demonlist/levels.js
@@ -0,0 +1,64 @@
+window.DB_LEVELS = [
+  {
+    "id": 1,
+    "name": "abyssal",
+    "difficulty": "Extreme Demon",
+    "creator": "raven",
+    "verifier": "Bboppa",
+    "levelId": "106",
+    "points": { "victor": "250.00", "verifier": "400.00" },
+    "victors": [{ "name": "carsonm", "percent": "100%" }],
+    "badgeBg": "from-[#facc15] to-[#d97706]"
+  },
+  {
+    "id": 2,
+    "name": "Unnamed 1",
+    "difficulty": "Insane Demon",
+    "creator": "Bboppa",
+    "verifier": "Bboppa",
+    "levelId": "111",
+    "points": { "victor": "236.41", "verifier": "386.41" },
+    "victors": [],
+    "badgeBg": "from-[#cbd5e1] to-[#64748b]"
+  },
+  {
+    "id": 3,
+    "name": "Explosions",
+    "difficulty": "Hard Demon",
+    "creator": "raven",
+    "verifier": "raven",
+    "levelId": "111",
+    "points": { "victor": "222.82", "verifier": "372.82" },
+    "victors": [],
+    "badgeBg": "from-[#d97706] to-[#9a3412]"
+  },
+  {
+    "id": 4,
+    "name": "Spectrogram",
+    "difficulty": "Medium Demon",
+    "creator": "raven",
+    "verifier": "raven",
+    "levelId": "109",
+    "points": { "victor": "209.23", "verifier": "359.23" },
+    "victors": [],
+    "badgeBg": "from-[#f43f5e] to-[#b91c1c]"
+  }
+];
+
+// Make sure that you do not change any of the file names or extensions!
+
+// Colors for #1 - #9: 
+//| #1 | `#facc15`| `#d97706`|
+//| #2 | `#cbd5e1`| `#64748b`|
+//| #3 | `#d97706`| `#9a3412`|
+//| #4 | `#f43f5e`| `#b91c1c`|
+//| #5 | `#3b82f6`| `#4338ca`|
+//| #6 | `#34d399`| `#0d9488`|
+//| #7 | `#a855f7`| `#3730a3`|
+//| #8 | `#3f3f46`| `#171717`|
+//| #9 | `#22d3ee`| `#3b82f6`|
+// cycle repeats
+
+
+
+
